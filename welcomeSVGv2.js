d21fca3df964e93a49a798b967594a4e97c200d8
argonathmos <65337143+argonathmos@users.noreply.github.com>
2020-05-29 11:05:16 +0200

    Update welcomeSVGv2.js
    Change to applyRandomColors():
    The colors used to be randomly picked from a list. 
    Now a random hex RGB code ('#000000') is generated:
    There is more colors available and the value of the <input type='color'> can be updated accordingly.

--- welcomeSVGv2.js
+++ welcomeSVGv2.js
@@ -48,167 +48,24 @@ function download(filename, data){
 
 
 // ----- RANDOM color generation -----
-const colorKeywords = ['black',
-'silver',
-'gray',
-'white',
-'maroon',
-'red',
-'purple',
-'fuchsia',
-'green',
-'lime',
-'olive',
-'yellow',
-'navy',
-'blue',
-'teal',
-'aqua',
-'orange',
-'aliceblue',
-'antiquewhite',
-'aquamarine',
-'azure',
-'beige',
-'bisque',
-'blanchedalmond',
-'blueviolet',
-'brown',
-'burlywood',
-'cadetblue',
-'chartreuse',
-'chocolate',
-'coral',
-'cornflowerblue',
-'cornsilk',
-'crimson',
-'cyan',
-'darkblue',
-'darkcyan',
-'darkgoldenrod',
-'darkgray',
-'darkgreen',
-'darkgrey',
-'darkkhaki',
-'darkmagenta',
-'darkolivegreen',
-'darkorange',
-'darkorchid',
-'darkred',
-'darksalmon',
-'darkseagreen',
-'darkslateblue',
-'darkslategray',
-'darkslategrey',
-'darkturquoise',
-'darkviolet',
-'deeppink',
-'deepskyblue',
-'dimgray',
-'dimgrey',
-'dodgerblue',
-'firebrick',
-'floralwhite',
-'forestgreen',
-'gainsboro',
-'ghostwhite',
-'gold',
-'goldenrod',
-'greenyellow',
-'grey',
-'honeydew',
-'hotpink',
-'indianred',
-'indigo',
-'ivory',
-'khaki',
-'lavender',
-'lavenderblush',
-'lawngreen',
-'lemonchiffon',
-'lightblue',
-'lightcoral',
-'lightcyan',
-'lightgoldenrodyellow',
-'lightgray',
-'lightgreen',
-'lightgrey',
-'lightpink',
-'lightsalmon',
-'lightseagreen',
-'lightskyblue',
-'lightslategray',
-'lightslategrey',
-'lightsteelblue',
-'lightyellow',
-'limegreen',
-'linen',
-'magenta',
-'mediumaquamarine',
-'mediumblue',
-'mediumorchid',
-'mediumpurple',
-'mediumseagreen',
-'mediumslateblue',
-'mediumspringgreen',
-'mediumturquoise',
-'mediumvioletred',
-'midnightblue',
-'mintcream',
-'mistyrose',
-'moccasin',
-'navajowhite',
-'oldlace',
-'olivedrab',
-'orangered',
-'orchid',
-'palegoldenrod',
-'palegreen',
-'paleturquoise',
-'palevioletred',
-'papayawhip',
-'peachpuff',
-'peru',
-'pink',
-'plum',
-'powderblue',
-'rosybrown',
-'royalblue',
-'saddlebrown',
-'salmon',
-'sandybrown',
-'seagreen',
-'seashell',
-'sienna',
-'skyblue',
-'slateblue',
-'slategray',
-'slategrey',
-'snow',
-'springgreen',
-'steelblue',
-'tan',
-'thistle',
-'tomato',
-'turquoise',
-'violet',
-'wheat',
-'whitesmoke',
-'yellowgreen',
-'rebeccapurple'];
 randomBtn.addEventListener('click', applyRandomColors);
 
-function randFromArray(a){
-    let index = Math.floor(Math.random()*a.length);
-    return a[index];
-};
-
 function applyRandomColors(){
-    let uniquePathcolor = randFromArray(colorKeywords);
-    cloud.setAttribute("stroke",randFromArray(colorKeywords));
-    cloud.setAttribute("fill",randFromArray(colorKeywords)); 
-    for(let i = 0; i<paths.length; i++){
-        paths[i].setAttribute("stroke", uniquePathcolor);
-    };
-};
+    let uniquePathcolor = '#'+((1<<24)*Math.random()|0).toString(16);
+    let strokeColor = '#'+((1<<24)*Math.random()|0).toString(16);
+    let fillColor = '#'+((1<<24)*Math.random()|0).toString(16);
 
+    cloud.setAttribute('stroke',strokeColor);
+    strokeColorInput.setAttribute('value',strokeColor);
+
+    cloud.setAttribute('fill', fillColor);
+    fillColorInput.setAttribute('value',fillColor);
+
+    for(let i = 0; i<paths.length; i++){
+        paths[i].setAttribute('stroke', uniquePathcolor);
+    }
+    pathColorInput.setAttribute('value', uniquePathcolor);
+
+}
+// Applying random colors everytime the page loads:
+applyRandomColors();
